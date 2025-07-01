48f02b93bc38aec5bb6d43550de204afd03211e7
x1nder <x1nder@proton.me>
2025-07-01 12:18:52 +0900

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,5 +1,32 @@
-from config import *
-import socket
+try:
+    from config import *
+    print("[+] Config.py file loaded successfully")
+except ImportError:
+    print("[-] Config file not found or not installed")
+
+try:
+    import socket
+    print("[+] Socket library is installed")
+except ImportError:
+    print("[-] Socket not installed (this is unusual, it's a built-in library)")
+
+try:
+    import sys
+    print("[+] Sys library is installed")
+except ImportError:
+    print("[-] Sys not installed (this is very unusual, it's a core Python library)")
+
+try:
+    import os
+    print("[+] Os library is installed")
+except ImportError:
+    print("[-] Os not installed (this is very unusual, it's a core Python library)")
+
+try:
+    import telebot
+    print("[+] Telebot library is installed")
+except ImportError:
+    print("[-] Telebot not installed")
 
 bunner = f'''
 ⢀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄
@@ -18,3 +45,8 @@ bunner = f'''
     version: {version_sofia}'''
 
 print(bunner)
+
+if os.getlogin() == "root":
+    print(f"Pshol nahui root")
+else:
+    print(f"Hello {os.getlogin()}")
